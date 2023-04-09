0a51d71c9c707c8adbad4a786d9333ea8079a647
Divit <divitajmera15@gmail.com>
2023-04-09 16:18:15 +0530

    added few designs to welcome page

--- src/Views/Welcome.jsx
+++ src/Views/Welcome.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import NavbarWelcome from "../components/NavbarWelcome";
+import { Link } from "react-router-dom";
 
 function Welcome() {
-  return <NavbarWelcome />;
+  return (
+    <>
+      <NavbarWelcome />
+      <div className="header-title">
+        <h1>Training and Placement Cell</h1>
+        <h1>IIT Patna</h1>
+      </div>
+      <div className="login-register-span">
+        <Link>
+          <span>Login</span>
+        </Link>
+        <Link>
+          <span>Register</span>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default Welcome;
--- src/components/NavbarWelcome.jsx
+++ src/components/NavbarWelcome.jsx
@@ -4,12 +4,16 @@ import { Link } from "react-router-dom";
 function NavbarWelcome() {
   return (
     <header>
-      <Link to="/">
+      <Link to="/" className="logo">
         <img src="" alt="logo" className="image nav-logo" />
       </Link>
       <nav className="nav-bar">
-        <div className="nav-item">About us</div>
-        <div className="nav-item">Team</div>
+        <Link to="/about">
+          <div className="nav-item">About us</div>
+        </Link>
+        <Link to="/Team">
+          <div className="nav-item">Team</div>
+        </Link>
       </nav>
     </header>
   );
